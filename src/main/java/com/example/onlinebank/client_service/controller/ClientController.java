d99b1483b0a0a83d18bbdecfdcedf1671a3a5b18
marishka_rish <litvinskaya.rish@gmail.com>
2024-07-28 16:36:24 +0300

    created client-service

---
+++ src/main/java/com/example/onlinebank/client_service/controller/ClientController.java
@@ -0,0 +1,57 @@
+package com.example.onlinebank.client_service.controller;
+
+import com.example.onlinebank.client_service.entity.Client;
+import com.example.onlinebank.client_service.exception.ClientExistsException;
+import com.example.onlinebank.client_service.exception.InvalidClientDataException;
+import com.example.onlinebank.client_service.service.ClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.notFound;
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientController {
+
+    private ClientService clientService;
+
+
+    @GetMapping
+    public List<Client> getAllClients() {
+        return clientService.getAllClients();
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long clientId) {
+        return clientService.getClientById(clientId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        try {
+            Client newClient = clientService.createClient(client);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newClient);
+        }catch (InvalidClientDataException e) {
+            return ResponseEntity.badRequest().build();
+        }catch (ClientExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Client> updateClient(@RequestBody Client client) {
+        Client updatedClient = clientService.updateClient(client);
+        return ResponseEntity.ok(updatedClient);
+    }
+
+    @DeleteMapping("/{clientId}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long clientId) {
+        clientService.deleteClientById(clientId);
+        return ResponseEntity.noContent().build();
+    }
+}
