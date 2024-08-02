75060d4bd243f738e1875bb239943d95a7f354e2
marishka_rish <litvinskaya.rish@gmail.com>
2024-08-02 18:29:25 +0300

    added validity check

--- src/main/java/com/example/onlinebank/card_service/servise/CardService.java
+++ src/main/java/com/example/onlinebank/card_service/servise/CardService.java
@@ -1,10 +1,17 @@
 package com.example.onlinebank.card_service.servise;
 
 import com.example.onlinebank.card_service.entity.Card;
+import com.example.onlinebank.card_service.entity.CardStatus;
+import com.example.onlinebank.card_service.exception.BalanceNotFoundException;
 import com.example.onlinebank.card_service.exception.CardNotFoundException;
+import com.example.onlinebank.card_service.exception.InvalidCardException;
 import com.example.onlinebank.card_service.repository.CardRepository;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,9 +27,10 @@ public class CardService {
         return cardRepository.findById(cardId).orElse(null);
     }
 
+    @SneakyThrows
     public Card createCard(Card card) {
-        if (card == null) {
-            throw new CardNotFoundException("Card cannot be null");
+        if (isValid(card)) {
+            throw new InvalidCardException("Card cannot be null");
         }
 
         Card newCard = new Card();
@@ -31,23 +39,59 @@ public class CardService {
         newCard.setCardNumber(card.getCardNumber());
         newCard.setExpirationDate(card.getExpirationDate());
         newCard.setCvv(card.getCvv());
+        newCard.setCardStatus(CardStatus.NEW);
         return cardRepository.save(newCard);
     }
 
     public Card updateCard(Long cardId, Card updatedCard) {
+        if (isValid(updatedCard)) {
+            throw new CardNotFoundException("Card cannot be null");
+        }
         Card existingCard = getCard(cardId);
-        if (existingCard != null) {
+        if (existingCard == null){
+            throw new CardNotFoundException("Card not found");
+        }
             existingCard.setCardType(updatedCard.getCardType());
             existingCard.setExpirationDate(updatedCard.getExpirationDate());
             existingCard.setCardNumber(updatedCard.getCardNumber());
             existingCard.setCvv(updatedCard.getCvv());
             return cardRepository.save(existingCard);
-        } else {
-            throw new CardNotFoundException("Card not found");
-        }
+
     }
 
     public void deleteCard(Long cardId) {
         cardRepository.deleteById(cardId);
+    }
+
+    public boolean isValid(Card card) {
+        if (card.getCardNumber() == null || card.getCardNumber().length() != 16 || !card.getCardNumber().matches("\\d+")) {
+            return false;
+        }
+        if ((card.getExpirationDate() == null) || card.getExpirationDate().before(Date.from(Instant.now()))){
+            return false;
+        }
+        if (card.getCvv() == null || card.getCvv().length() != 3 || !card.getCvv().matches("\\d+")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @SneakyThrows
+    public BigDecimal getBalance(Long cardId) {
+        Card card = getCard(cardId);
+        if (card == null) {
+            throw new CardNotFoundException("Card not found");
+        }
+        if (card.getBalance() == null) {
+            throw new BalanceNotFoundException("Balance is null");
+        }
+        return card.getBalance();
+    }
+    public boolean isBlocked(Long cardId) {
+        Card card = cardRepository.findById(cardId).orElseThrow(()-> new CardNotFoundException("Card not found"));
+        card.setCardStatus(CardStatus.BLOCKED);
+        cardRepository.save(card);
+        return true;
     }
 }
--- src/main/java/com/example/onlinebank/payment_service/service/PaymentService.java
+++ src/main/java/com/example/onlinebank/payment_service/service/PaymentService.java
@@ -1,24 +1,43 @@
 package com.example.onlinebank.payment_service.service;
 
+import com.example.onlinebank.card_service.entity.Card;
+import com.example.onlinebank.card_service.exception.CardNotFoundException;
+import com.example.onlinebank.card_service.servise.CardService;
 import com.example.onlinebank.payment_service.entity.Payment;
+import com.example.onlinebank.payment_service.entity.PaymentStatus;
 import com.example.onlinebank.payment_service.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class PaymentService {
 
-    PaymentRepository paymentRepository;
+    private PaymentRepository paymentRepository;
+    @Autowired
+    private CardService cardService;
 
 
     public Payment createPayment(Payment payment) {
+        Card card = cardService.getCard(payment.getCardId());
+        if (!isValidCardForTransaction(card, payment.getAmount())){
+            throw new CardNotFoundException("Invalid card or insufficient found");
+        }
         return paymentRepository.save(payment);
+    }
+
+    private boolean isValidCardForTransaction(Card card, BigDecimal amount) {
+        return cardService.isValid(card)
+                && card.getBalance().compareTo(amount) >= 0
+                &&!cardService.isBlocked(card.getId());
     }
 
     public Payment getPayment(Long id) {
         return paymentRepository.findById(id).orElseThrow();
     }
 
-    public Payment updatePaymentStatus(Long id, String status) {
+    public Payment updatePaymentStatus(Long id, PaymentStatus status) {
         Payment payment = getPayment(id);
         payment.setPaymentStatus(status);
         return paymentRepository.save(payment);
