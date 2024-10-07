1d242091c35aa2f98a2c012ec8c96485f47b7af0
Marina <112541429+MarishkaIT@users.noreply.github.com>
2024-10-07 19:30:06 +0200

    refactoring

--- src/main/java/com/example/onlinebank/card_service/servise/CardService.java
+++ src/main/java/com/example/onlinebank/card_service/servise/CardService.java
@@ -8,11 +8,14 @@ import com.example.onlinebank.card_service.exception.InvalidCardException;
 import com.example.onlinebank.card_service.repository.CardRepository;
 import com.example.onlinebank.notification_service.entity.Notification;
 import com.example.onlinebank.notification_service.service.NotificationService;
+import com.example.onlinebank.security_service.EncryptService;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -21,6 +24,7 @@ public class CardService {
 
     CardRepository cardRepository;
     NotificationService notificationService;
+    EncryptService encryptService;
 
     public List<Card> getAllCards() {
         return cardRepository.findAll();
@@ -32,34 +36,37 @@ public class CardService {
 
     @SneakyThrows
     public Card createCard(Card card) {
-        if (isValid(card)) {
+        if (!isValid(card)) {
             throw new InvalidCardException("Card cannot be null");
         }
 
         Card newCard = new Card();
-        newCard.setClient(card.getClient());
-        newCard.setCardType(card.getCardType());
-        newCard.setCardNumber(card.getCardNumber());
-        newCard.setExpirationDate(card.getExpirationDate());
-        newCard.setCvv(card.getCvv());
+        setCardProperties(card, newCard);
+
+        SecretKey key = encryptService.getSecretKey();
+        byte[] encryptedCardNumber = encryptService.encrypt(card.getCardNumber().getBytes(), key);
+        byte[] encryptedCvv = encryptService.encrypt(card.getCvv().getBytes(), key);
+
+        newCard.setCardNumber(Arrays.toString(encryptedCardNumber));
+        newCard.setCvv(Arrays.toString(encryptedCvv));
+
+
         newCard.setCardStatus(CardStatus.NEW);
         notificationService.sendNotification(new Notification("Card created", "Your card hes created successfully", newCard.getClient().getId()));
         return cardRepository.save(newCard);
     }
 
+
     public Card updateCard(Long cardId, Card updatedCard) {
-        if (isValid(updatedCard)) {
+        if (!isValid(updatedCard)) {
             throw new CardNotFoundException("Card cannot be null");
         }
         Card existingCard = getCard(cardId);
         if (existingCard == null){
             throw new CardNotFoundException("Card not found");
         }
-            existingCard.setCardType(updatedCard.getCardType());
-            existingCard.setExpirationDate(updatedCard.getExpirationDate());
-            existingCard.setCardNumber(updatedCard.getCardNumber());
-            existingCard.setCvv(updatedCard.getCvv());
-            return cardRepository.save(existingCard);
+        setCardProperties(existingCard, updatedCard);
+        return cardRepository.save(existingCard);
 
     }
 
@@ -68,13 +75,13 @@ public class CardService {
     }
 
     public boolean isValid(Card card) {
-        if (card.getCardNumber() == null || card.getCardNumber().length() != 16 || !card.getCardNumber().matches("\\d+")) {
+        if (card.getCardNumber() == null || card.getCardNumber().trim().isEmpty() || card.getCardNumber().length() != 16 || !card.getCardNumber().matches("\\d+")) {
             return false;
         }
         if ((card.getExpirationDate() == null) || card.getExpirationDate().before(Date.from(Instant.now()))){
             return false;
         }
-        if (card.getCvv() == null || card.getCvv().length() != 3 || !card.getCvv().matches("\\d+")) {
+        if (card.getCvv() == null || card.getCvv().trim().isEmpty() || card.getCvv().length() != 3 || !card.getCvv().matches("\\d+")) {
             return false;
         }
 
@@ -87,4 +94,13 @@ public class CardService {
         cardRepository.save(card);
         return true;
     }
+
+    private static void setCardProperties(Card card, Card source) {
+        source.setClient(card.getClient());
+        source.setCardType(card.getCardType());
+        source.setCardNumber(card.getCardNumber());
+        source.setExpirationDate(card.getExpirationDate());
+        source.setCvv(card.getCvv());
+    }
+
 }
